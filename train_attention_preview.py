8ebfd51a1939d0083238db0b23721b9be8ee9635
Alice-XiaolinZhu <57396196+Alice-XiaolinZhu@users.noreply.github.com>
2022-06-14 23:34:16 +0100

    Update train_attention_preview.py

--- train_attention_preview.py
+++ train_attention_preview.py
@@ -323,7 +323,7 @@ def backward(loss, action_logprob, fixatedFraction, printHere=True):
         torch.nn.utils.clip_grad_norm_(parameters(), clip_bound, norm_type=clip_type)
     optimizer.step()
     
-my_save_path = f"./models/attention_{WITH_CONTEXT}_{WITH_LM}_{previewLength}_{degradedNoise}.ckpt"
+my_save_path = f"./models/attention_{args.WITH_CONTEXT}_{args.WITH_LM}_{args.previewLength}_{args.degradedNoise}.ckpt"
 def SAVE():
        torch.save({"devRewards" : devRewards, "args" : args, "components_lm" : [x.state_dict() for x in components_lm], "components_attention" : [x.state_dict() for x in components_attention], "learning_rate" : learning_rate}, my_save_path)
 
@@ -370,7 +370,7 @@ for epoch in range(4):
         print("Mean valid loss:", sum(validLoss)/examplesNumber)
         print("Mean valid reward:", sum(validReward)/examplesNumber)
         
-        with open(f"./results/train_attention_{WITH_CONTEXT}_{WITH_LM}_{previewLength}_{degradedNoise}_result.txt", "w") as outFile:
+        with open(f"./results/train_attention_{args.WITH_CONTEXT}_{args.WITH_LM}_{args.previewLength}_{args.degradedNoise}_result.txt", "w") as outFile:
             #print(args, file=outFile)
             #print(devAccuracies, file=outFile)
             print(devLosses, file=outFile)
