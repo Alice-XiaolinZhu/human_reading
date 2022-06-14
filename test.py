7424b1cf8a7897898e815d3aee0ebfd98f25ae21
Alice-XiaolinZhu <57396196+Alice-XiaolinZhu@users.noreply.github.com>
2022-06-14 23:39:09 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,10 @@
-import torch
+'''import torch
 print(torch.__version__)
-print(torch.cuda.is_available())
+print(torch.cuda.is_available())'''
+
+
+
+params = [[True, True, 3, True], [True, True, 3, False], [False, True, 3, True]]
+for param in params:
+  with_context, with_lm, preview_length, degraded_noise = param
+  
