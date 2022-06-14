7eb0f195f3b8da44adc46824bc8461740be9603e
Alice-XiaolinZhu <57396196+Alice-XiaolinZhu@users.noreply.github.com>
2022-06-14 23:44:48 +0100

    Update test.py

--- test.py
+++ test.py
@@ -2,9 +2,9 @@
 print(torch.__version__)
 print(torch.cuda.is_available())'''
 
-
+import os
 
 params = [[True, True, 3, True], [True, True, 3, False], [False, True, 3, True]]
 for param in params:
   with_context, with_lm, preview_length, degraded_noise = param
-  
+  os.system(f'python test_attention_preview.py --WITH_CONTEXT {with_context} --WITH_LM {with_lm} --previewLength {preview_length} --degradedNoise {degraded_noise}')
