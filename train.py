baee7b277977ce604b47c7dab5d62569c33dce8b
Alice-XiaolinZhu <57396196+Alice-XiaolinZhu@users.noreply.github.com>
2022-06-15 00:01:31 +0100

    Update train.py

--- train.py
+++ train.py
@@ -1,9 +1,9 @@
 import os
-os.system('python train_attention_basic.py')
+os.system('python train_attention_basic.py --batchSize 32 --learning_rate 0.1 --dropout 0.1')
 
 params = [[True, True, 3, True], [True, True, 3, False], [False, True, 3, True]]
 for param in params:
     with_context, with_lm, preview_length, degraded_noise = param
     print("Train on parameters:", with_context, with_lm, preview_length, degraded_noise)
-    os.system(f'python train_attention_preview.py --WITH_CONTEXT {with_context} --WITH_LM {with_lm} --previewLength {preview_length} --degradedNoise {degraded_noise}')
-    os.system(f'python train_attention_saccadic_length.py --WITH_CONTEXT {with_context} --WITH_LM {with_lm} --previewLength {preview_length} --degradedNoise {degraded_noise}')
+    os.system(f'python train_attention_preview.py --batchSize 32 --learning_rate 0.1 --dropout 0.1 --WITH_CONTEXT {with_context} --WITH_LM {with_lm} --previewLength {preview_length} --degradedNoise {degraded_noise}')
+    os.system(f'python train_attention_saccadic_length.py --batchSize 32 --learning_rate 0.1 --dropout 0.1 --WITH_CONTEXT {with_context} --WITH_LM {with_lm} --previewLength {preview_length} --degradedNoise {degraded_noise}')
