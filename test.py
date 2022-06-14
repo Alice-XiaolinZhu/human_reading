f6af4b4ad83ad087a05463f6154a0aba5b06f08c
Alice-XiaolinZhu <57396196+Alice-XiaolinZhu@users.noreply.github.com>
2022-06-14 23:59:14 +0100

    Update test.py

--- test.py
+++ test.py
@@ -8,5 +8,6 @@ os.system('python test_attention_basic.py')
 params = [[True, True, 3, True], [True, True, 3, False], [False, True, 3, True]]
 for param in params:
     with_context, with_lm, preview_length, degraded_noise = param
+    print("Test on parameters:", with_context, with_lm, preview_length, degraded_noise)
     os.system(f'python test_attention_preview.py --WITH_CONTEXT {with_context} --WITH_LM {with_lm} --previewLength {preview_length} --degradedNoise {degraded_noise}')
     os.system(f'python test_attention_saccadic_length.py --WITH_CONTEXT {with_context} --WITH_LM {with_lm} --previewLength {preview_length} --degradedNoise {degraded_noise}')
