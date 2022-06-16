6e18c808e15096be6a28b47899b3e49486631b58
Alice-XiaolinZhu <57396196+Alice-XiaolinZhu@users.noreply.github.com>
2022-06-16 01:31:45 +0100

    Update train.py

--- train.py
+++ train.py
@@ -1,11 +1,12 @@
 import os
 
-print("Batch size, learning rate, dropout, embedding:", 32, 0.1, 0.1, "CWE")
-os.system('python train_attention_basic.py --batchSize 32 --learning_rate 0.1 --dropout 0.1 --embedding_used "CWE"')
+embedding_used = "None"
+print("Batch size, learning rate, dropout, embedding:", 32, 0.1, 0.1, embedding_used)
+os.system('python train_attention_basic.py --batchSize 32 --learning_rate 0.1 --dropout 0.1 --embedding_used {embedding_used}')
 
 params = [[True, True, 3, True], [True, True, 3, False], [False, True, 3, True]]
 for param in params:
     with_context, with_lm, preview_length, degraded_noise = param
     print("Train on parameters:", with_context, with_lm, preview_length, degraded_noise)
-    os.system(f'python train_attention_preview.py --batchSize 32 --learning_rate 0.1 --dropout 0.1 --WITH_CONTEXT {with_context} --WITH_LM {with_lm} --previewLength {preview_length} --degradedNoise {degraded_noise} --embedding_used "CWE"')
-    os.system(f'python train_attention_saccadic_length.py --batchSize 32 --learning_rate 0.1 --dropout 0.1 --WITH_CONTEXT {with_context} --WITH_LM {with_lm} --previewLength {preview_length} --degradedNoise {degraded_noise} --embedding_used "CWE"')
+    os.system(f'python train_attention_preview.py --batchSize 32 --learning_rate 0.1 --dropout 0.1 --WITH_CONTEXT {with_context} --WITH_LM {with_lm} --previewLength {preview_length} --degradedNoise {degraded_noise} --embedding_used {embedding_used}')
+    os.system(f'python train_attention_saccadic_length.py --batchSize 32 --learning_rate 0.1 --dropout 0.1 --WITH_CONTEXT {with_context} --WITH_LM {with_lm} --previewLength {preview_length} --degradedNoise {degraded_noise} --embedding_used {embedding_used}')
