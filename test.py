c808e626462e1e21553eca332b87707d8b990cb3
Alice-XiaolinZhu <57396196+Alice-XiaolinZhu@users.noreply.github.com>
2022-06-19 23:27:00 +0100

    Update test.py

--- test.py
+++ test.py
@@ -10,9 +10,9 @@ params = [[True, True, 3, False]] #, [True, True, 3, False], [False, True, 3, Tr
 for lambda_ in lambdas:
     os.system(f'python test_attention_basic.py --embedding_used {embedding_used} --LAMBDA {lambda_}')
 
-    for param in params:
+    '''for param in params:
         with_context, with_lm, preview_length, degraded_noise = param
         print("Test on parameters:", with_context, with_lm, preview_length, degraded_noise, lambda_)
         os.system(f'python test_attention_preview.py --WITH_CONTEXT {with_context} --WITH_LM {with_lm} --previewLength {preview_length} --degradedNoise {degraded_noise} --embedding_used {embedding_used} --LAMBDA {lambda_}')
-        os.system(f'python test_attention_saccadic_length.py --WITH_CONTEXT {with_context} --WITH_LM {with_lm} --previewLength {preview_length} --degradedNoise {degraded_noise} --embedding_used {embedding_used} --LAMBDA {lambda_}')
+        os.system(f'python test_attention_saccadic_length.py --WITH_CONTEXT {with_context} --WITH_LM {with_lm} --previewLength {preview_length} --degradedNoise {degraded_noise} --embedding_used {embedding_used} --LAMBDA {lambda_}')'''
     
