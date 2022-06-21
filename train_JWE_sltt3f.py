b4fde066c4201d8e84696eabcc8b7dde62fe7140
Alice-XiaolinZhu <57396196+Alice-XiaolinZhu@users.noreply.github.com>
2022-06-21 03:34:29 +0100

    Create train_JWE_sltt3f.py

---
+++ train_JWE_sltt3f.py
@@ -0,0 +1,15 @@
+import os
+
+embedding_used = "JWE"
+print("Batch size, learning rate, dropout, embedding:", 128, 0.1, 0.1, embedding_used)
+
+lambdas = [3.0, 3.5, 4.0, 4.5, 5.0] #[1.5, 2, 2.5]
+params = [[True, True, 3, False]] #, [True, True, 3, False], [False, True, 3, True]]
+
+for lambda_ in lambdas:
+    #os.system(f'python train_attention_basic.py --batchSize 128 --learning_rate 0.1 --dropout 0.1 --embedding_used {embedding_used} --LAMBDA {lambda_}')
+    for param in params:
+        with_context, with_lm, preview_length, degraded_noise = param
+        print("Train on parameters:", with_context, with_lm, preview_length, degraded_noise, lambda_)
+        #os.system(f'python train_attention_preview.py --batchSize 128 --learning_rate 0.1 --dropout 0.1 --WITH_CONTEXT {with_context} --WITH_LM {with_lm} --previewLength {preview_length} --degradedNoise {degraded_noise} --embedding_used {embedding_used} --LAMBDA {lambda_}')
+        os.system(f'python train_attention_saccadic_length.py --batchSize 128 --learning_rate 0.1 --dropout 0.1 --WITH_CONTEXT {with_context} --WITH_LM {with_lm} --previewLength {preview_length} --degradedNoise {degraded_noise} --embedding_used {embedding_used} --LAMBDA {lambda_}')
