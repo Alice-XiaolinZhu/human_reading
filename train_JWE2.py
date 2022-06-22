88af9f5a702074ea1029aafc11f2e2b41c3891b8
Alice-XiaolinZhu <57396196+Alice-XiaolinZhu@users.noreply.github.com>
2022-06-22 21:24:04 +0100

    Update train_JWE2.py

--- train_JWE2.py
+++ train_JWE2.py
@@ -3,8 +3,8 @@ import os
 embedding_used = "JWE"
 print("Batch size, learning rate, dropout, embedding:", 128, 0.1, 0.1, embedding_used)
 
-lambdas = [2.5, 2.625, 2.875, 3.125, 3.375, 3.625, 3.875, 4.125, 4.375, 4.625, 4.875, 5.125, 5.375, 5.5] #[3.125, 3.375, 3.625, 3.875, 4.125, 4.375, 4.625, 4.875, 5.125, 5.375, 5.625, 5.875] #[3.5, 4, 4.5, 5, 5.5] #[1.5, 2, 2.5]
-params = [[True, True, 3, False]] #, [True, True, 3, False], [False, True, 3, True]]
+lambdas = [3.0, 3.5, 4.0, 4.5, 5.0] #[2.5, 2.625, 2.875, 3.125, 3.375, 3.625, 3.875, 4.125, 4.375, 4.625, 4.875, 5.125, 5.375, 5.5] #[3.125, 3.375, 3.625, 3.875, 4.125, 4.375, 4.625, 4.875, 5.125, 5.375, 5.625, 5.875] #[3.5, 4, 4.5, 5, 5.5] #[1.5, 2, 2.5]
+params = [[False, True, 3, False]] #, [True, True, 3, False], [False, True, 3, True]]
 
 for lambda_ in lambdas:
     #os.system(f'python train_attention_basic.py --batchSize 128 --learning_rate 0.1 --dropout 0.1 --embedding_used {embedding_used} --LAMBDA {lambda_}')
