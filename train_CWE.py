5fa576d11b67d784c619b999c629692e1a426526
Alice-XiaolinZhu <57396196+Alice-XiaolinZhu@users.noreply.github.com>
2022-06-23 23:05:55 +0100

    Update train_CWE.py

--- train_CWE.py
+++ train_CWE.py
@@ -3,7 +3,7 @@ import os
 embedding_used = "CWE"
 print("Batch size, learning rate, dropout, embedding:", 128, 0.1, 0.1, embedding_used)
 
-lambdas = [3.0, 3.5, 4.0, 4.5, 5.0] #[2.625, 2.875, 3.125, 3.375, 3.625, 3.875, 4.125, 4.375, 4.625, 4.875, 5.125, 5.375] #[2.5, 2.75, 3.0, 3.25, 3.75, 4.25, 4.75, 5.25] #[3.5, 4, 4.5, 5, 5.5]
+lambdas = [5.0] #[2.625, 2.875, 3.125, 3.375, 3.625, 3.875, 4.125, 4.375, 4.625, 4.875, 5.125, 5.375] #[2.5, 2.75, 3.0, 3.25, 3.75, 4.25, 4.75, 5.25] #[3.5, 4, 4.5, 5, 5.5]
 params = [[False, False, 3, False]] #, [True, True, 3, False], [False, True, 3, True]]
 
 for lambda_ in lambdas:
