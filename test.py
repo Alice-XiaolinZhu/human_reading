e742baddad8126931b6cd4dda74e39c53bbb9cee
Alice-XiaolinZhu <57396196+Alice-XiaolinZhu@users.noreply.github.com>
2022-06-24 10:04:05 +0100

    Update test.py

--- test.py
+++ test.py
@@ -6,8 +6,8 @@ import os
 import numpy as np
 embedding_useds = ["None", "CWE", "JWE"] # "None"
 
-lambdas = list(np.array([*range(2500, 5325, 125)])/1000) #[3.125, 3.375, 3.625, 3.875, 4.125, 4.375, 4.625, 4.875, 5.125, 5.375, 5.625, 5.875] #[3.0, 3.5, 4.0, 4.5, 5.0] #[2.625, 2.875, 3.125, 3.375, 3.625, 3.875, 4.125, 4.375, 4.625, 4.875, 5.125] #[1.5, 2, 2.5] #[3.5, 4, 4.5, 5, 5.5] #[1.5, 2, 2.5]
-params = [[False, True, 3, False]] #, [True, True, 3, False], [False, True, 3, True]]
+lambdas = list(np.array([*range(2500, 5500, 125)])/1000) #[3.125, 3.375, 3.625, 3.875, 4.125, 4.375, 4.625, 4.875, 5.125, 5.375, 5.625, 5.875] #[3.0, 3.5, 4.0, 4.5, 5.0] #[2.625, 2.875, 3.125, 3.375, 3.625, 3.875, 4.125, 4.375, 4.625, 4.875, 5.125] #[1.5, 2, 2.5] #[3.5, 4, 4.5, 5, 5.5] #[1.5, 2, 2.5]
+params = [[True, True, 3, False]] #, [True, True, 3, False], [False, True, 3, True]]
 
 for embedding_used in embedding_useds:
     for lambda_ in lambdas:
@@ -16,6 +16,6 @@ for embedding_used in embedding_useds:
         for param in params:
             with_context, with_lm, preview_length, degraded_noise = param
             print("Test on parameters:", with_context, with_lm, preview_length, degraded_noise, lambda_)
-            os.system(f'python test_attention_preview.py --WITH_CONTEXT {with_context} --WITH_LM {with_lm} --previewLength {preview_length} --degradedNoise {degraded_noise} --embedding_used {embedding_used} --LAMBDA {lambda_}')
-            #os.system(f'python test_attention_saccadic_length.py --WITH_CONTEXT {with_context} --WITH_LM {with_lm} --previewLength {preview_length} --degradedNoise {degraded_noise} --embedding_used {embedding_used} --LAMBDA {lambda_}')
+            #os.system(f'python test_attention_preview.py --WITH_CONTEXT {with_context} --WITH_LM {with_lm} --previewLength {preview_length} --degradedNoise {degraded_noise} --embedding_used {embedding_used} --LAMBDA {lambda_}')
+            os.system(f'python test_attention_saccadic_length.py --WITH_CONTEXT {with_context} --WITH_LM {with_lm} --previewLength {preview_length} --degradedNoise {degraded_noise} --embedding_used {embedding_used} --LAMBDA {lambda_}')
     
