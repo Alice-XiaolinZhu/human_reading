17b4c8f2a60f791650ebfeb7084b24513f5ef889
Alice-XiaolinZhu <57396196+Alice-XiaolinZhu@users.noreply.github.com>
2022-07-14 23:29:50 +0100

    Update train_None.py

--- train_None.py
+++ train_None.py
@@ -3,13 +3,20 @@ import os
 embedding_used = "None"
 print("Batch size, learning rate, dropout, embedding:", 128, 0.1, 0.1, embedding_used)
 
-lambdas = [3.0, 3.125, 3.375, 3.625, 3.875, 4.125, 4.375, 4.625, 4.875, 5.125, 5.375] #[3.0, 3.25, 3.75, 4.25, 4.75, 5.25, 5.75, 6.0] #[1.5, 2, 2.5]
-params = [[False, False, 3, False]] #, [True, True, 3, False], [False, True, 3, True]]
+lambdas = list(np.array([*range(3000, 6250, 250)])/1000)
+reward_factors = [0.01, 0.05, 0.1, 0.15, 0.2, 0.25]
+entropy_weights = [0.001, 0.005, 0.01, 0.015, 0.02]
+
+#params = [[False, False, 3, False]] #, [True, True, 3, False], [False, True, 3, True]]
 
 for lambda_ in lambdas:
-    os.system(f'python train_attention_basic.py --batchSize 128 --learning_rate 0.1 --dropout 0.1 --embedding_used {embedding_used} --LAMBDA {lambda_}')
-    for param in params:
-        with_context, with_lm, preview_length, degraded_noise = param
-        print("Train on parameters:", with_context, with_lm, preview_length, degraded_noise, lambda_)
-        #os.system(f'python train_attention_preview.py --batchSize 128 --learning_rate 0.1 --dropout 0.1 --WITH_CONTEXT {with_context} --WITH_LM {with_lm} --previewLength {preview_length} --degradedNoise {degraded_noise} --embedding_used {embedding_used} --LAMBDA {lambda_}')
-        #os.system(f'python train_attention_saccadic_length.py --batchSize 128 --learning_rate 0.1 --dropout 0.1 --WITH_CONTEXT {with_context} --WITH_LM {with_lm} --previewLength {preview_length} --degradedNoise {degraded_noise} --embedding_used {embedding_used} --LAMBDA {lambda_}')
+    for reward_factor_ in reward_factors:
+        for entropy_weight_ in entropy_weights:
+            print("Train on parameters:", lambda_, reward_factor_, entropy_weight_)
+            os.system(f'python train_attention_basic.py --batchSize 128 --learning_rate 0.1 --dropout 0.1 --embedding_used {embedding_used} --LAMBDA {lambda_} --REWARD_FACTOR {reward_factor_} --ENTROPY_WEIGHT {entropy_weight_}')
+            
+            #for param in params:
+                #with_context, with_lm, preview_length, degraded_noise = param
+                #print("Train on parameters:", with_context, with_lm, preview_length, degraded_noise, lambda_)
+                #os.system(f'python train_attention_preview.py --batchSize 128 --learning_rate 0.1 --dropout 0.1 --WITH_CONTEXT {with_context} --WITH_LM {with_lm} --previewLength {preview_length} --degradedNoise {degraded_noise} --embedding_used {embedding_used} --LAMBDA {lambda_}')
+                #os.system(f'python train_attention_saccadic_length.py --batchSize 128 --learning_rate 0.1 --dropout 0.1 --WITH_CONTEXT {with_context} --WITH_LM {with_lm} --previewLength {preview_length} --degradedNoise {degraded_noise} --embedding_used {embedding_used} --LAMBDA {lambda_}')
