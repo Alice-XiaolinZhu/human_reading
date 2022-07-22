d2e741445aeb8d49b8e7301144a19b57dfef61a5
Alice-XiaolinZhu <57396196+Alice-XiaolinZhu@users.noreply.github.com>
2022-07-22 18:08:02 +0100

    Update train_None2.py

--- train_None2.py
+++ train_None2.py
@@ -3,8 +3,8 @@ import os
 embedding_used = "None"
 print("Batch size, learning rate, dropout, embedding:", 128, 0.1, 0.1, embedding_used)
 
-lambdas = [2.5, 2.625, 2.875, 3.125, 3.375, 3.625, 3.875, 4.125, 4.375, 4.625, 4.875, 5.125, 5.375, 5.5] #[3.125, 3.375, 3.625, 3.875, 4.125, 4.375, 4.625, 4.875, 5.125, 5.375, 5.625, 5.875] #[1.5, 2, 2.5]
-params = [[False, True, 3, False]] #, [True, True, 3, False], [False, True, 3, True]]
+lambdas = [2.5] #[3.125, 3.375, 3.625, 3.875, 4.125, 4.375, 4.625, 4.875, 5.125, 5.375, 5.625, 5.875] #[1.5, 2, 2.5]
+params = [[True, False, 0, True]] #, [True, True, 3, False], [False, True, 3, True]]
 
 for lambda_ in lambdas:
     #os.system(f'python train_attention_basic.py --batchSize 128 --learning_rate 0.1 --dropout 0.1 --embedding_used {embedding_used} --LAMBDA {lambda_}')
