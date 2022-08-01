3772d503f7fda7b618d31bf69aa828b6d23694d3
Alice-XiaolinZhu <57396196+Alice-XiaolinZhu@users.noreply.github.com>
2022-08-01 02:14:34 +0100

    Update train_CWE6.py

--- train_CWE6.py
+++ train_CWE6.py
@@ -4,8 +4,8 @@ import numpy as np
 embedding_used = "CWE"
 print("Batch size, learning rate, dropout, embedding:", 128, 0.1, 0.1, embedding_used)
 
-lambdas = [4.5] #list(np.array([*range(5250, 5750, 250)])/1000)
-reward_factors = [0.01, 0.05, 0.1, 0.15, 0.2, 0.25]
+lambdas = [4.25] #list(np.array([*range(5250, 5750, 250)])/1000)
+reward_factors = [0.1, 0.15, 0.2, 0.25] #[0.01, 0.05, 0.1, 0.15, 0.2, 0.25]
 entropy_weights = [0.001, 0.005, 0.01, 0.015, 0.02]
 
 #params = [[False, False, 3, False]] #, [True, True, 3, False], [False, True, 3, True]]
