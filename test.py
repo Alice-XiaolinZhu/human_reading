abb272cd80cf87e997e0902f953d5117b0d7fedf
Alice-XiaolinZhu <57396196+Alice-XiaolinZhu@users.noreply.github.com>
2022-08-05 08:01:31 +0100

    Update test.py

--- test.py
+++ test.py
@@ -12,7 +12,7 @@ gaussian_var3 = 0.5
 lambdas = list(np.array([*range(3000, 6250, 250)])/1000)
 reward_factors = [0.25] #[0.01, 0.05, 0.1, 0.15, 0.2, 0.25]
 entropy_weights = [0.001] #[0.001, 0.005, 0.01, 0.015, 0.02]
-params = [[True, False, 0, False], [False, True, 0, False], [True, True, 0, False], [False, False, 3, False], [False, False, 3, True]] 
+params = [[True, False, 3, False], [False, True, 3, False], [True, True, 3, False], [False, False, 3, False], [False, False, 3, True]] 
 
 for embedding_used in embedding_useds:
     for lambda_ in lambdas:
@@ -24,8 +24,9 @@ for embedding_used in embedding_useds:
                     with_context, with_lm, preview_length, degraded_noise = param
                     print("Test on parameters:", with_context, with_lm, preview_length, degraded_noise, lambda_)
                     if preview_length == 0:
-                        os.system(f'python test_attention_preview.py --WITH_CONTEXT {with_context} --WITH_LM {with_lm} --previewLength {preview_length} --degradedNoise {degraded_noise} --embedding_used {embedding_used} --LAMBDA {lambda_} --REWARD_FACTOR {reward_factor_} --ENTROPY_WEIGHT {entropy_weight_}')
+                        #os.system(f'python test_attention_preview.py --WITH_CONTEXT {with_context} --WITH_LM {with_lm} --previewLength {preview_length} --degradedNoise {degraded_noise} --embedding_used {embedding_used} --LAMBDA {lambda_} --REWARD_FACTOR {reward_factor_} --ENTROPY_WEIGHT {entropy_weight_}')
+                        os.system(f'python test_attention_saccadic_length.py --WITH_CONTEXT {with_context} --WITH_LM {with_lm} --previewLength {preview_length} --degradedNoise {degraded_noise} --embedding_used {embedding_used} --LAMBDA {lambda_} --REWARD_FACTOR {reward_factor_} --ENTROPY_WEIGHT {entropy_weight_}')
                     elif preview_length == 3:
-                        os.system(f'python test_attention_preview.py --WITH_CONTEXT {with_context} --WITH_LM {with_lm} --previewLength {preview_length} --degradedNoise {degraded_noise} --gaussianVars {gaussian_var1} {gaussian_var2} {gaussian_var3} --embedding_used {embedding_used} --LAMBDA {lambda_} --REWARD_FACTOR {reward_factor_} --ENTROPY_WEIGHT {entropy_weight_}')
-                    #os.system(f'python test_attention_saccadic_length.py --WITH_CONTEXT {with_context} --WITH_LM {with_lm} --previewLength {preview_length} --degradedNoise {degraded_noise} --embedding_used {embedding_used} --LAMBDA {lambda_}')
+                        #os.system(f'python test_attention_preview.py --WITH_CONTEXT {with_context} --WITH_LM {with_lm} --previewLength {preview_length} --degradedNoise {degraded_noise} --gaussianVars {gaussian_var1} {gaussian_var2} {gaussian_var3} --embedding_used {embedding_used} --LAMBDA {lambda_} --REWARD_FACTOR {reward_factor_} --ENTROPY_WEIGHT {entropy_weight_}')
+                        os.system(f'python test_attention_saccadic_length.py --WITH_CONTEXT {with_context} --WITH_LM {with_lm} --previewLength {preview_length} --degradedNoise {degraded_noise} --gaussianVars {gaussian_var1} {gaussian_var2} {gaussian_var3} --embedding_used {embedding_used} --LAMBDA {lambda_} --REWARD_FACTOR {reward_factor_} --ENTROPY_WEIGHT {entropy_weight_}')
     
