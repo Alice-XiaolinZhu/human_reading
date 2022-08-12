309753964f772faf5dda1990d6152d56a71a1139
Alice-XiaolinZhu <57396196+Alice-XiaolinZhu@users.noreply.github.com>
2022-08-12 09:54:58 +0100

    Update train_attention_saccadic_length.py

--- train_attention_saccadic_length.py
+++ train_attention_saccadic_length.py
@@ -269,13 +269,13 @@ def forward(batch, calculateAccuracy=False):
     else:
         # Collect target values for decoding loss
         targets = texts.transpose(0,1).contiguous()[1:]
-        print("??????", targets.type())
+        print("??????", torch.FloatTensor(targets.cpu().detach().numpy()).cuda().type())
         print("??????", torch.ones(mask.size()).cuda().type())
         print("??????", torch.LongTensor(attentionDecisions.cpu().detach().numpy()).cuda().type())
         print("??????", saccade_history.type())
         print("??????", torch.zeros(attentionDecisions.size()).cuda().type())
         print("??????", torch.zeros(mask.size()).cuda().type())
-        targets = torch.where(torch.LongTensor(attentionDecisions.cpu().detach().numpy()).cuda() == 1.0, targets, torch.zeros(attentionDecisions.size()).cuda()) # 0: mask
+        targets = torch.where(torch.LongTensor(attentionDecisions.cpu().detach().numpy()).cuda() == 1.0, torch.FloatTensor(targets.cpu().detach().numpy()).cuda(), torch.zeros(attentionDecisions.size()).cuda()) # 0: mask
         outputs_cat = output(outputs_decoder)
     loss = crossEntropy(outputs_cat.view(-1, 50004), targets.view(-1)).view(outputs_cat.size()[0], outputs_cat.size()[1])
     
