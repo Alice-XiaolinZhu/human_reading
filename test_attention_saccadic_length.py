dadaf1c6b9475f2a8377885231f00086ea978443
Alice-XiaolinZhu <57396196+Alice-XiaolinZhu@users.noreply.github.com>
2022-08-12 12:06:22 +0100

    Update test_attention_saccadic_length.py

--- test_attention_saccadic_length.py
+++ test_attention_saccadic_length.py
@@ -341,7 +341,7 @@ with open(f"./results/test_attention_SL_{args.WITH_CONTEXT}_{args.WITH_LM}_{args
     examplesNumber = 0
     counter = 1
     TEXT_ = []
-    batches = list(loadCorpus("validation", batchSize))
+    batches = list(loadCorpus("testing", batchSize))
     print("Number of batches", len(batches))
     for batch in batches:
         with torch.no_grad():
