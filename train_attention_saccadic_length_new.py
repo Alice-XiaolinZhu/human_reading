2040329bc06d63012d490d64b3a4b6e078494047
Alice-XiaolinZhu <57396196+Alice-XiaolinZhu@users.noreply.github.com>
2022-08-12 07:49:05 +0100

    Update train_attention_saccadic_length_new.py

--- train_attention_saccadic_length_new.py
+++ train_attention_saccadic_length_new.py
@@ -253,20 +253,23 @@ def forward(batch, calculateAccuracy=False):
 
     embedded = char_embeddings(texts).transpose(0,1)
     outputs_decoder, _ = reconstructor(embedded[:-1], hidden)
-    print("attentionDecisions size:", attentionDecisions.size())
-    print("outputs_decoder size:", outputs_decoder.size())
-    print("output(outputs_decoder) size:", output(outputs_decoder).size())
-    print("targets size:", texts.transpose(0,1).contiguous()[1:].size())
+    #print("attentionDecisions size:", attentionDecisions.size()) #[31,128]
+    #print("outputs_decoder size:", outputs_decoder.size()) #[31,128,1024]
+    #print("output(outputs_decoder) size:", output(outputs_decoder).size()) #[31,128,50004]
+    #print("targets size:", texts.transpose(0,1).contiguous()[1:].size()) #[31,128]
        
     if WITH_LM:
         # Collect target values for both surprisal and decoding loss
+        # mask targets by attentionDecisions for computing loss 
         targets = texts.transpose(0,1)
+        targets = torch.where(attentionDecisions == 1, targets, torch.zeros(attentionDecisions.size()).cuda()) # 0: mask
         targets = torch.cat([targets[1:], targets[1:]], dim=0)
         outputs_reader = torch.cat(outputs, dim=0)
         outputs_cat = output(torch.cat([outputs_reader, outputs_decoder], dim=0))
     else:
         # Collect target values for decoding loss
         targets = texts.transpose(0,1).contiguous()
+        targets = torch.where(attentionDecisions == 1, targets, torch.zeros(attentionDecisions.size()).cuda()) # 0: mask
         targets = targets[1:]
         outputs_cat = output(outputs_decoder)
     loss = crossEntropy(outputs_cat.view(-1, 50004), targets.view(-1)).view(outputs_cat.size()[0], outputs_cat.size()[1])
