aee6a06983490538a8a906b600a4cc80e7306ec3
Alice-XiaolinZhu <57396196+Alice-XiaolinZhu@users.noreply.github.com>
2022-08-14 03:57:10 +0100

    Update train_None_preview.py

--- train_None_preview.py
+++ train_None_preview.py
@@ -2,7 +2,7 @@ import os
 import numpy as np
 
 embedding_used = "None"
-gaussian_var1 = 0.02
+gaussian_var1 = 0.0
 gaussian_var2 = 0.1
 gaussian_var3 = 0.5
 
@@ -11,7 +11,7 @@ print("Batch size, learning rate, dropout, embedding:", 128, 0.1, 0.1, embedding
 lambdas = list(np.array([*range(3000, 6250, 250)])/1000)
 reward_factors = [0.25] #[0.01, 0.05, 0.1, 0.15, 0.2, 0.25]
 entropy_weights = [0.001] #[0.001, 0.005, 0.01, 0.015, 0.02]
-params = [[False, True, 0, False]] # with context, with lm, degraded noise
+params = [[False, True, 1, False]] # with context, with lm, degraded noise
 
 for lambda_ in lambdas:
     for reward_factor_ in reward_factors:
